d0d324ce164eaaf73a3654e946492c9eab1d666d
AriRice <97973849+AriRice@users.noreply.github.com>
2024-04-15 15:54:07 -0500

    Update Step_04_coverage.sh

--- Step_04_coverage.sh
+++ Step_04_coverage.sh
@@ -8,24 +8,23 @@
 #SBATCH --mail-user=arrice@ttu.edu
 #SBATCH --mail-type=ALL
 
-. ~/conda/etc/profile.d/conda.sh
-conda activate samtools
+module load intel/18.0.3.222 samtools/1.9
 
 samtools depth -a M1_luciae_1_final.bam M3_crissalis_1_final.bam M4_crissalis_2_final.bam M5_crissalis_3_final.bam \
-M6_celata_1_final.bam M7_crissalis_4_final.bam M8_peregrina_1_final.bam M9_peregrina_2_final.bam M10_peregrina_3_final.bam \
+M6_celata_1_final.bam M7_crissalis_4_final.bam M8_peregrina_1_final.bam M9_peregrina_2_final.bam \
 M11_ruficapilla_1_final.bam M12_ruficapilla_2_final.bam M13_celata_2_final.bam M14_peregrina_4_final.bam M15_celata_3_final.bam \
-M16_ruficapilla_3_final.bam M17_ruficapilla_4_final.bam M18_peregrina_5_final.bam M19_celata_4_final.bam M20_celata_5_final.bam \
+M17_ruficapilla_4_final.bam M18_peregrina_5_final.bam M19_celata_4_final.bam M20_celata_5_final.bam \
 M21_virginiae_1_final.bam M22_crissalis_5_final.bam M23_luciae_2_final.bam M24_luciae_3_final.bam M25_luciae_4_final.bam \
 M26_virginiae_2_final.bam M27_virginiae_3_final.bam M28_virginiae_4_final.bam M29_virginiae_5_final.bam \
 M30_luciae_5_final.bam G_poliocephala_KU25093_final.bam G_poliocephala_KU33403_final.bam \
 G_poliocephala_KU33414_final.bam G_poliocephala_KU33542_final.bam \
-G_poliocephala_KU9041_final.bam N16_peregrina_6_final.bam N17_peregrina_7_final.bam N18_ruficapilla_6_final.bam \ 
+G_poliocephala_KU9041_final.bam N16_peregrina_6_final.bam N17_peregrina_7_final.bam N18_ruficapilla_6_final.bam \
 N19_ruficapilla_7_final.bam N20_ruficapilla_8_final.bam N21_ruficapilla_9_final.bam > leiothlypis_coverage.txt
 
 # break up the depth files into single column files for each individual
 
 while read -r name1 number1; do
-	number2=$((number1 + 2));
+        number2=$((number1 + 2));
   cut leiothlypis_coverage.txt -f $number2 > ${name1}_depth.txt;
 done < popmap.txt
 
