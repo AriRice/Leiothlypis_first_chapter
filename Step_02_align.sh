4ba558d6e3535efeea6c4c753261c635cafc9a12
AriRice <97973849+AriRice@users.noreply.github.com>
2024-04-18 11:04:29 -0500

    Update Step_02_align.sh

--- Step_02_align.sh
+++ Step_02_align.sh
@@ -38,7 +38,7 @@ refgenome=${workdir}/00_ref_genome/ncbi_dataset/data/GCA_009764595.1/GCA_0097645
 # "tpe" Trims both reads to the same length in case a kmer was only detected in one of them. 
 
 # define the location of the reference mitogenomes
-mito=${workdir}/01b_mtDNA/songbird_mitogenomes.fasta
+mito=${workdir}/01b_mtDNA/songbird_pero_mitogenomes.fasta
 
 # run bbsplit
 /lustre/work/jmanthey/bbmap/bbsplit.sh in1=${workdir}/01_cleaned/${basename_array}_R1.fastq.gz in2=${workdir}/01_cleaned/${basename_array}_R2.fastq.gz ref=${mito} basename=${workdir}/01b_mtDNA/${basename_array}_%.fastq.gz outu1=${workdir}/01b_mtDNA/${basename_array}_R1.fastq.gz outu2=${workdir}/01b_mtDNA/${basename_array}_R2.fastq.gz
