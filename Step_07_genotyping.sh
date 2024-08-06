4622ff66393986d06427976228edd9d7f03cb471
AriRice <97973849+AriRice@users.noreply.github.com>
2024-08-06 14:32:34 -0500

    Update Step_07_genotyping.sh

--- Step_07_genotyping.sh
+++ Step_07_genotyping.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --chdir=./
 #SBATCH --job-name=genotype
-#SBATCH --partition quanah
+#SBATCH --partition nocona
 #SBATCH --time=48:00:00
 #SBATCH --nodes=1 --ntasks=4
 #SBATCH --time=48:00:00
