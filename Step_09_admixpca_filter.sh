e6c42f14e62b97bf3cb1f54ca6fa8551146db1ed
AriRice <97973849+AriRice@users.noreply.github.com>
2024-11-01 15:04:45 -0500

    Update Step_09_admixpca_filter.sh

--- Step_09_admixpca_filter.sh
+++ Step_09_admixpca_filter.sh
@@ -19,10 +19,10 @@ workdir=/lustre/scratch/arrice/Ch1_Leiothlypis
 region_array=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/scaffolds.txt | tail -n1 )
 
 # filtering for admixture analyses and PCA- thinned
-vcftools --vcf ${workdir}/04_filtered_vcf/${region_array}.vcf --remove outgroup.txt --max-missing 1 --minGQ 20 --minDP 6 --max-meanDP 40 --min-alleles 2 --max-alleles 2 --mac 2 --thin 50000 --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/05_admixture_and_pca/${region_array}__structure_50kbpthin
+vcftools --vcf ${workdir}/04_filtered_vcf/${region_array}.vcf --remove outgroup_and_excluded_samples.txt --max-missing 1 --minGQ 20 --minDP 6 --max-meanDP 40 --min-alleles 2 --max-alleles 2 --mac 2 --thin 50000 --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/05_admixture_and_pca/${region_array}__structure_50kbpthin
 
 # filtering for admixture analyses and PCA- not thinned
-vcftools --vcf ${workdir}/04_filtered_vcf/${region_array}.vcf --remove outgroup.txt --max-missing 1 --minGQ 20 --minDP 6 --max-meanDP 40 --min-alleles 2 --max-alleles 2 --mac 2  --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/05_admixture_and_pca/${region_array}__structure_nothin
+vcftools --vcf ${workdir}/04_filtered_vcf/${region_array}.vcf --remove outgroup_and_excluded_samples.txt --max-missing 1 --minGQ 20 --minDP 6 --max-meanDP 40 --min-alleles 2 --max-alleles 2 --mac 2  --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/05_admixture_and_pca/${region_array}__structure_nothin
 
 bgzip ${workdir}/05_admixture_and_pca/${region_array}__structure_nothin.recode.vcf
 bgzip ${workdir}/05_admixture_and_pca/${region_array}__structure_50kbpthin.recode.vcf
