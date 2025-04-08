caeea4723c54672e0fd00e0f2a6fbf8367b4bd52
AriRice <97973849+AriRice@users.noreply.github.com>
2025-04-07 19:27:02 -0500

    Create run_introgression_chrom.sh

---
+++ D_statistics/run_introgression_chrom.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=D_and_fD_chrom
+#SBATCH --nodes=1 --ntasks=1
+#SBATCH --partition nocona
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --mail-user=arrice@ttu.edu
+#SBATCH --mail-type=ALL
+#SBATCH --array=1-32
+
+workdir=/lustre/scratch/arrice/Ch1_Leiothlypis/11_ABBA_BABA/fd_vs_D/chrom_level
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate phylostats_env
+
+R --no-save
+
+input_array=$( head -n${SLURM_ARRAY_TASK_ID} ../scaffolds.txt | tail -n1 )
+
+Rscript Chrom_level_D_and_fD_stats.r ${workdir}/${input_array}__ABBA_BABA.simple.vcf ../leio_introgression_popmap.txt ../leio_introgression_comparisons.txt
